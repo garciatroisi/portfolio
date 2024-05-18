bc2f9a36788330884bd51b0ddc1eac57d332d1c0
Diego Garcia <garciatroisi@gmail.com>
2024-05-18 08:06:31 -0300

    Book Appointment

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -9,16 +9,23 @@ const Contact = () => {
       </p>
       <h2 className="font-titleFont text-5xl font-semibold">Get In Touch</h2>
       <p className="max-w-[600px] text-center text-textDark">
-        I&apos;m always open to exploring new challenges and opportunities where 
-        I can contribute as a developer or consultant. Feel free to reach
-        out if you have any questions or simply want to connect.
+        I&apos;m always open to exploring new challenges and opportunities where
+        I can contribute as a developer or consultant. Feel free to reach out if
+        you have any questions or simply want to connect.
       </p>
 
-      <a href="mailto:garciatroisi@gmail.com">
-        <button className="w-40 h-14 border border-textGreen mt-6 font-titleFont text-sm text-textGreen tracking-wider rounded-md hover:bg-hoverColor duration-300">
-          Say Hello
-        </button>
-      </a>
+      <div>
+        <a href="mailto:garciatroisi@gmail.com">
+          <button className="w-40 h-14 border border-textGreen font-titleFont text-sm text-textGreen tracking-wider rounded-md hover:bg-hoverColor duration-300">
+            Say Hello
+          </button>
+        </a>
+        <a href="https://calendly.com/garciatroisi" target="_blank">
+          <button className="ml-4 w-40 h-14 border border-textGreen font-titleFont text-sm text-textGreen tracking-wider rounded-md hover:bg-hoverColor duration-300">
+            Book Appointment
+          </button>
+        </a>
+      </div>
     </section>
   );
 };
