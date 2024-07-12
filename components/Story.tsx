cb40f4f65459a92317ed4e192913a4abcbfe5328
Diego Garcia <garciatroisi@gmail.com>
2024-07-12 15:51:21 -0300

    translate story

--- components/Story.tsx
+++ components/Story.tsx
@@ -10,7 +10,7 @@ const Story = () => {
       id="story"
       className="max-w-containerSmall mx-auto py-10 lgl:py-32 flex flex-col gap-8"
     >
-      <SectionTitle title="Our Story" titleNo="01" />
+      <SectionTitle title="Sobre PFG" titleNo="03" />
       <div className="flex flex-col lgl:flex-row gap-16">
         <div className="w-full lgl:w-2/3 text-base text-textDark font-medium flex flex-col gap-4">
           <p>
